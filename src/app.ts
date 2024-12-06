43f1a5a8fa88f3f2583540134a8b66f3b2dd8ab8
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-12-06 12:30:35 +0330

    chore: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import cors from "cors";
 import globalErrorHandler from "@/controllers/error.controller";
 import userRoute from "@/routes/user.route";
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 app.use("/api/v1/users", userRoute);
 
