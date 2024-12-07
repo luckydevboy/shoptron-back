d0d571b3f192ea5a4ca13c5153f7ae1520099bbe
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-12-07 09:30:12 +0330

    fix: don't show password just for get all users api

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import User from "@/models/user.model";
 import catchAsync from "@/utils/catchAsync";
 
 export const getUsers = catchAsync(async (req, res) => {
-  const users = await User.find();
+  const users = await User.find().select("-password");
 
   res.status(200).json({
     status: "success",
